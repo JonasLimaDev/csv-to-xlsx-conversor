094d7ae941b80ce212abba73559d7a73582f34a1
Jonas Lima <jonas.lima.dev@gmail.com>
2024-10-04 09:57:07 -0300

    separação do projeto em arquivos de módulos

--- modulos_xlsx.py
+++ modulos_xlsx.py
@@ -1,4 +1,3 @@
-import csv
 import xlsxwriter
 import string
 
@@ -64,33 +63,6 @@ def get_dict_letters(limit):
     return letters_dict
 
 
-def get_data_csv_file(path_csv_file="dados.csv"):
-    """
-    Lê um arquivo '.csv' e retornar os dados em uma lista de listas,
-    sendo, as listas internas as linhas do arquivo.
-
-    Entrada:
-     - Caminho do arquivo csv que será lido.
-     - type: str
-
-    Saída:
-     - lista de listas com os dados do arquivo lido.
-     - type: list
-
-    Exemplo da estrutura de saída:
-    [
-        ['linha 1 coluna A', 'linha 1 coluna B'],
-        ['linha 2 coluna A', 'linha 2 coluna B'],
-    ]
-
-    """
-    list_data_file = []
-    with open(path_csv_file, 'r',  encoding='latin-1') as file_csv:
-        csv_file_reader = csv.reader(file_csv)
-        for row in csv_file_reader:
-            list_data_file.append(row)
-    return list_data_file
-
 
 def create_xlsx_file(data_to_save):
 
@@ -104,5 +76,3 @@ def create_xlsx_file(data_to_save):
         row_index += 1
     workbook.close()
 
-
-create_xlsx_file(get_data_csv_file())
