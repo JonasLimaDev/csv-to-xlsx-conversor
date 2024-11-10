c0f06d5dfbe2c480b17310375f9dbfcf9277f16e
Jonas Lima <jonas.lima.dev@gmail.com>
2024-11-10 09:42:18 -0300

    remoção de arquivos desnecessários

--- main-cli.py
+++
@@ -1,6 +0,0 @@
-from conversor.class_data_csv import *
-from conversor.classes_configuration_file import *
-from conversor.functions_configuration_file import *
-from conversor.functions_data_csv import *
-from conversor.modulos_xlsx import *
-from conversor.modulos_csv_with_pandas import *
